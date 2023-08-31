4072b272340b928b05293377b1eb227af92eba92
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-08-31 11:41:31 -0600

    Extensión de métodos de clases

---
+++ Modelo/ClaseExtensiones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    // La clase donde se guardan las extensiones
+    // debe ser estática
+    public static class ClaseExtensiones
+    {
+        // El método que extiende debe ser estático
+        // El primer parámetro lleva this y
+        // representa el tipo que estamos extendiendo
+        public static bool EsPar(this int i)
+        {
+            if(i % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        // Método que extiende a un double
+        public static double Duplica(this double d)
+        {
+            return d * 2.0;
+        }
+    }
+}
---
+++ PruebaMetodosExtension/Program.cs
@@ -0,0 +1,14 @@
+﻿// Este programa muestra el uso de métodos de
+// extensión
+using Modelo;
+
+int numero = 55;
+bool par = numero.EsPar(); // Aquí encontramos
+                           // la extensión
+
+Console.WriteLine("{0} es {1}", numero, par);
+
+// Extensión del double
+double valor = 55.18;
+
+Console.WriteLine(valor.Duplica());
