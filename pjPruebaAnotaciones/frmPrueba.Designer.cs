4917a63bf18fd37abe27a4e58536e7b15ba66ead
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-08-31 11:22:42 -0600

    pjPruebaAnotaciones

---
+++ pjPruebaAnotaciones/frmPrueba.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace pjPruebaAnotaciones
+{
+    partial class frmPrueba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIDNumber = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id Number:";
+            // 
+            // txtIDNumber
+            // 
+            this.txtIDNumber.Location = new System.Drawing.Point(177, 33);
+            this.txtIDNumber.Name = "txtIDNumber";
+            this.txtIDNumber.Size = new System.Drawing.Size(150, 31);
+            this.txtIDNumber.TabIndex = 1;
+            this.txtIDNumber.Text = "9999999";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(177, 102);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(150, 31);
+            this.txtFirstName.TabIndex = 3;
+            this.txtFirstName.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(177, 172);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(150, 31);
+            this.txtLastName.TabIndex = 5;
+            this.txtLastName.Text = "k";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(256, 250);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 34);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmPrueba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 329);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIDNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrueba";
+            this.Text = "frmPrueba";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtIDNumber;
+        private TextBox txtFirstName;
+        private Label label2;
+        private TextBox txtLastName;
+        private Label label3;
+        private Button btnSave;
+    }
+}
---
+++ pjPruebaAnotaciones/frmPrueba.cs
@@ -0,0 +1,51 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjPruebaAnotaciones
+{
+    public partial class frmPrueba : Form
+    {
+        Employee objE = new();
+        public frmPrueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objE.IdNumber = int.Parse(txtIDNumber.Text);
+            }
+            catch (ValidationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                objE.FirstName = txtFirstName.Text;
+            }
+            catch (ValidationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                objE.LastName = txtLastName.Text;
+            }
+            catch (ValidationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
